131d0768b1c1246e38a67e5571ce0b5d80292eb5
Benzza9899 <sipphapasp9947@gmail.com>
2024-03-08 13:00:28 +0000

    Code smell fix

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
-require './lib/route_recognizer.rb'
+require './lib/route_recognizer'
 
-Result = Struct.new(:route, :controller, :action, :params, :error)
-
+# RouteRecognizerApp is a Sinatra application for recognizing routes.
 class RouteRecognizerApp < Sinatra::Base
-
   get '/' do
     @router = RouteRecognizer.new('')
     @result = nil
@@ -15,16 +15,16 @@ class RouteRecognizerApp < Sinatra::Base
     @routes = params[:routes_table_text].to_s
     @result = Result.new
     method = params[:route_method].upcase
-    @is_post = %w(POST PUT PATCH).include?(method)
+    @is_post = %w[POST PUT PATCH].include?(method)
     @result.route = "#{method} #{params[:route_uri]}"
-    logger.info %Q{::#{@result.route}::#{@routes.gsub("\n"," ; ")}}
+    logger.info %(::#{@result.route}::#{@routes.gsub("\n", ' ; ')})
     begin
       @router = RouteRecognizer.new(@routes)
       result_params = @router.recognize(params[:route_method], params[:route_uri])
       @result.controller = result_params.delete(:controller)
       @result.action = result_params.delete(:action)
       @result.params = result_params
-    rescue RouteRecognizer::NoMatchingRouteError => e
+    rescue RouteRecognizer::NoMatchingRouteError
       @result.error = "doesn't match any of the route patterns above."
     rescue RouteRecognizer::InvalidRoutesError => e
       @result.error = "can't be parsed because your routes table (top of page) seems to contain an error: #{e.message}"
--- lib/route_recognizer.rb
+++ lib/route_recognizer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# RouteRecognizer is responsible for recognizing routes.
 class RouteRecognizer
   require 'rails'
 
@@ -5,55 +8,59 @@ class RouteRecognizer
   class NoMatchingRouteError < StandardError; end
 
   attr_accessor :router, :routes_as_string
-  def initialize(string='')
+
+  def initialize(string = '')
     @routes_as_string = string
     @routes = ActionDispatch::Routing::RouteSet.new
-    begin
-      @routes.draw { eval string } unless string.blank?
-    rescue Exception => e       # yes, really, because anything can happen inside an Eval
-      raise InvalidRoutesError, e.message
-    end
+    @routes.draw { instance_eval(string) } unless string.blank?
+  rescue StandardError => e
+    raise InvalidRoutesError, e.message
   end
 
-  def recognize(method,uri)
-    rack_request = RackRequest.new(method,uri)
+  def recognize(method, uri)
+    rack_request = RackRequest.new(method, uri)
     request = ActionDispatch::Request.new(rack_request.env)
     all_params = nil
-    @routes.router.recognize(request) do |route,params|
+    @routes.router.recognize(request) do |_route, params|
       all_params = request.query_parameters.merge params
     end
     raise NoMatchingRouteError unless all_params
+
     all_params
   end
 
-  
-  class RackRequest
-    attr_accessor :method, :uri, :query_string, :path, :env
-    def initialize(method,uri)
-      @method,@uri = method,uri
-      set_query_string!
-      set_env!
-    end
-    
-    private
+  module RouteRecognizer
+    # RackRequest is responsible for handling rack requests.
+    class RackRequest
+      attr_accessor :method, :uri, :query_string, :path, :env
 
-    def set_env!
-      @env = {
-        'REQUEST_URI' => uri,
-        'PATH_INFO' => path,
-        'REQUEST_METHOD' => method
-      }
-      @env.merge!({'QUERY_STRING' => query_string}) if query_string
-    end
+      def initialize(method, uri)
+        @method = method
+        @uri = uri
+        set_query_string!
+        set_env!
+      end
 
-    def set_query_string!
-      if @uri =~ /(.+)\?(.+)\Z/
-        @path,@query_string = $1,$2
-      else                        # no query string
-        @path = @uri
-        @query_string = nil
+      private
+
+      def set_env!
+        @env = {
+          'REQUEST_URI' => uri,
+          'PATH_INFO' => path,
+          'REQUEST_METHOD' => method
+        }
+        @env.merge!({ 'QUERY_STRING' => query_string }) if query_string
+      end
+
+      def set_query_string!
+        if @uri =~ /(.+)\?(.+)\Z/
+          @path = ::Regexp.last_match(1)
+          @query_string = ::Regexp.last_match(2)
+        else # no query string
+          @path = @uri
+          @query_string = nil
+        end
       end
     end
-
   end
 end
